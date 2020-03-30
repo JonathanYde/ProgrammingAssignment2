ebd722f8051203d28a31e0b7d6f0936f7695acb5
JonathanYde <jonathan.yde@biomed.au.dk>
2020-03-30 21:43:13 +0200

    creates the makeCacheMatrix function

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,21 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+##### FIRST EXERCISE #####
 
+# I create the makeCacheMatrix function, which can store a matrix and calculate the the inverse matrix (solve the matrix)
+makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        } # the 'set' function is used to load a matrix into 'makeCacheMatrix'
+        get <- function() x # the 'get' function is used to return a matrix loaded into 'makeCacheMatrix'
+        setsolve <- function(solve) m <<- solve # the 'setsolve' function calculates the inverse matrix
+        getsolve <- function() m # the 'getsolve' function returns the inverse matrix
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
